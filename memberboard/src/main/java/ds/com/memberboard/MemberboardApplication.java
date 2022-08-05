152bcb0c4810f61312f93a939bf250277be5d015
SimDav888 <m.simardavid@gmail.com>
2022-08-05 09:48:40 +0900

    first commit

---
+++ memberboard/src/main/java/ds/com/memberboard/MemberboardApplication.java
@@ -0,0 +1,13 @@
+package ds.com.memberboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberboardApplication.class, args);
+	}
+
+}
